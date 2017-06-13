8ded793e671f247a8ef1ad567d04b088e9a80e25
Keebin <kirikliu@gmail.com>
2017-06-12 17:46:25 -0700

    Add checkRow helper

--- index.js
+++ index.js
@@ -8,17 +8,42 @@ let currPlayer = 0;
 
 const generateSchemaPerTurn = (playerIdx) => {
   return [{
-    description: `${players[playerIdx]}, enter a column`,
-    name: 'column',
-    type: 'number'
-  }, {
     description: `${players[playerIdx]}, enter a row`,
     name: 'row',
+    type: 'number'
+  }, {
+    description: `${players[playerIdx]}, enter a column`,
+    name: 'column',
     type: 'number'
   }]
 }
 
-let gameOn = true;
+const checkRow = () => {
+  let currRow = [' ', ' ', ' '];
+  return board.reduce(function(won, row) {
+    won[0] = row[0];
+    console.log(won, row);
+    if (won[1] || row[0] === ' ') return won;
+    return row.reduce(function(won, col, i) {
+      if (!won[1] && i > 0) {
+        return won;
+      }
+      return [won[0], won[0] === col];
+    }, won)
+  }, [' ', false])
+}
+
+const checkCol = () => {
+
+}
+
+const checkDiag = () => {
+
+}
+
+const checkIfWon = () => {
+  return checkRow();
+}
 
 const printBoard = () => {
   board.forEach(function(row, i) {
@@ -34,10 +59,16 @@ printBoard();
 
 const turn = () => prompt.get(generateSchemaPerTurn(currPlayer), function(err, result) {
   if (err) return err;
-  currPlayer = currPlayer === 0 ? 1 : 0;
-  board[result.row][result.column] = 'X';
+  board[result.row][result.column] = currPlayer === 0 ? 'X' : 'O';
+  const winner = checkIfWon();
   printBoard();
-  turn(generateSchemaPerTurn(currPlayer));
+  if (winner[1]) {
+    console.log(`${players[currPlayer]} wins!`)
+    return;
+  } else {
+    currPlayer = currPlayer === 0 ? 1 : 0;
+    turn(generateSchemaPerTurn(currPlayer));
+  }
 });
 
 turn();
